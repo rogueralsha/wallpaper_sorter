e6d14b33dada84fe0e9b250f562a8b9460b1a035
rogueralsha <rogueralsha@gmail.com>
2016-12-08 14:37:01 -0600

    no message

---
+++ WallpaperSorter/MainWindow.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WallpaperSorter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Regex tagRegex = new Regex(@"\d+ - ([^\.]+)");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sourceText.Text = Properties.Settings.Default.SourceDir;
+            destinationText.Text = Properties.Settings.Default.DesinationDir;
+        }
+
+        private void sourceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.SelectedPath = Properties.Settings.Default.SourceDir;
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            if (result != System.Windows.Forms.DialogResult.Cancel)
+                Properties.Settings.Default.SourceDir = dialog.SelectedPath;
+            sourceText.Text = Properties.Settings.Default.SourceDir;
+            Properties.Settings.Default.Save();
+        }
+
+        private void destinationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.SelectedPath = Properties.Settings.Default.DesinationDir;
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            if (result != System.Windows.Forms.DialogResult.Cancel)
+                Properties.Settings.Default.DesinationDir = dialog.SelectedPath;
+            destinationText.Text = Properties.Settings.Default.DesinationDir;
+            Properties.Settings.Default.Save();
+        }
+
+        private void goButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                process();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private String findArtistTag(String[] tags)
+        {
+            const String ARTIST_TAG_PREFIX = "artist_";
+            const String ARTIST_TAG_PREFIX_2 = "artist-";
+            const String ARTIST_TAG_SUFFIX_PARENTHETICAL = "_(artist)";
+            foreach (String tag in tags)
+            {
+                if (tag.ToLower().StartsWith(ARTIST_TAG_PREFIX))
+                {
+                    return tag.Substring(ARTIST_TAG_PREFIX.Length);
+                }
+                if (tag.ToLower().StartsWith(ARTIST_TAG_PREFIX_2))
+                {
+                    return tag.Substring(ARTIST_TAG_PREFIX_2.Length);
+                }
+                if (tag.ToLower().EndsWith(ARTIST_TAG_SUFFIX_PARENTHETICAL))
+                {
+                    return tag.Substring(0, tag.Length - ARTIST_TAG_SUFFIX_PARENTHETICAL.Length);
+                }
+            }
+            return String.Empty;
+        }
+
+        private void process()
+        {
+            Dictionary<string, List<String>> directoriesNamesAndArtists = new Dictionary<string, List<string>>();
+            List<string> skippedArtists = new List<string>();
+
+            DirectoryInfo destinationDir = new DirectoryInfo(Properties.Settings.Default.DesinationDir);
+
+
+            DirectoryInfo[] categoryDirs = destinationDir.GetDirectories();
+            foreach (DirectoryInfo categoryDir in categoryDirs)
+            {
+                string category = categoryDir.Name.ToLower();
+                directoriesNamesAndArtists.Add(category, new List<string>());
+
+                DirectoryInfo[] artistDirs = categoryDir.GetDirectories();
+                foreach (DirectoryInfo artistDir in artistDirs)
+                {
+                    if(artistDir.Name=="skribblix")
+                    {
+                        Console.Out.WriteLine();
+                    }
+                    directoriesNamesAndArtists[category].Add(artistDir.Name.ToLower());
+                    processArtistDir(artistDir);
+                }
+            }
+
+            DirectoryInfo sourceDir = new DirectoryInfo(Properties.Settings.Default.SourceDir);
+            FileInfo[] remainingFiles = sourceDir.GetFiles();
+
+            foreach (FileInfo f in remainingFiles)
+            {
+                f.Refresh();
+                if (!f.Exists)
+                    continue;
+                String name = correctFileName(f.Name);
+                String[] tags = getTags(name);
+                String artistTag = findArtistTag(tags);
+
+                if (String.IsNullOrWhiteSpace(artistTag))
+                {
+                    List<FileInfo> tmp = new List<FileInfo>();
+
+                    tmp.Add(f);
+                    TagPicker tagPicker = new TagPicker(tags, tmp);
+                    tagPicker.Owner = this;
+                    if (tagPicker.ShowDialog().Value && !String.IsNullOrWhiteSpace(tagPicker.SelectedTag))
+                    {
+                        artistTag = tagPicker.SelectedTag.Trim().ToLower();
+                    }
+                }
+
+                if (String.IsNullOrWhiteSpace(artistTag))
+                {
+                    continue;
+                }
+
+                artistTag = artistTag.ToLower().Trim();
+
+                String artistCategory = String.Empty;
+
+                foreach(String category in directoriesNamesAndArtists.Keys)
+                {
+                    if(directoriesNamesAndArtists[category].Contains(artistTag))
+                    {
+                        artistCategory = category;
+                        break;
+                    }
+                }
+
+                if(String.IsNullOrEmpty(artistCategory))
+                {
+                    List<FileInfo> artistImages = getOtherArtistImages(artistTag);
+                    
+                    TagPicker tagPicker = new TagPicker(directoriesNamesAndArtists.Keys.ToArray<String>(), artistImages);
+                    tagPicker.Owner = this;
+                    if (tagPicker.ShowDialog().Value && !String.IsNullOrWhiteSpace(tagPicker.SelectedTag))
+                    {
+                        artistCategory = tagPicker.SelectedTag;
+                    }
+                }
+
+                if (String.IsNullOrWhiteSpace(artistCategory))
+                {
+                    continue;
+                }
+
+                if (!directoriesNamesAndArtists.ContainsKey(artistCategory))
+                    directoriesNamesAndArtists.Add(artistCategory, new List<string>());
+
+                if (!directoriesNamesAndArtists[artistCategory].Contains(artistTag))
+                    directoriesNamesAndArtists[artistCategory].Add(artistTag);
+
+                String newArtistDir = System.IO.Path.Combine(destinationDir.FullName, System.IO.Path.Combine(artistCategory, artistTag));
+                DirectoryInfo newArtistDirInfo = new DirectoryInfo(newArtistDir);
+                if (!newArtistDirInfo.Exists)
+                    newArtistDirInfo.Create();
+
+                processArtistDir(newArtistDirInfo);
+            }
+        }
+
+
+        private String[] getTags(String name)
+        {
+            if (!tagRegex.IsMatch(name))
+                throw new Exception("No regex match: " + name);
+
+            String[] tags = tagRegex.Match(name).Groups[1].Value.Split(' ');
+            for(int i = 0; i<tags.Length; i++)
+            {
+                tags[i] = tags[i].Trim().Trim('-').ToLower();
+            }
+            return tags;
+        }
+
+        private List<FileInfo> getOtherArtistImages(String artistName)
+        {
+            List<FileInfo> output = new List<FileInfo>();
+            artistName = artistName.ToLower();
+            DirectoryInfo sourceDir = new DirectoryInfo(Properties.Settings.Default.SourceDir);
+            FileInfo[] matchingFiles = sourceDir.GetFiles();
+            foreach (FileInfo f in matchingFiles)
+            {
+                String name = correctFileName(f.Name);
+                String[] tags = getTags(name);
+                if (!tags.Contains(artistName) && findArtistTag(tags) != artistName)
+                    continue;
+                output.Add(f);
+            }
+            return output;
+        }
+
+        private void processArtistDir(DirectoryInfo artistDir)
+        {
+            List<FileInfo> matchingFiles = getOtherArtistImages(artistDir.Name);
+
+            foreach (FileInfo f in matchingFiles)
+            {
+                String name = correctFileName(f.Name);
+
+                String destination = System.IO.Path.Combine(artistDir.FullName, name);
+
+                if (File.Exists(destination))
+                    throw new Exception("File already exists: " + destination);
+
+                f.MoveTo(destination);
+            }
+        }
+
+        private String correctFileName(String input)
+        {
+            return System.Web.HttpUtility.UrlDecode(input).Replace(":", "_");
+        }
+    }
+}
---
+++ WallpaperSorter/TagPicker.xaml.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WallpaperSorter
+{
+    public class PreviewImage
+    {
+        public BitmapImage Image { get; set; }
+        public String Name { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for TagPicker.xaml
+    /// </summary>
+    public partial class TagPicker : Window
+    {
+        public List<PreviewImage> ImagePreviews { get; set; }
+
+        public int ImageColumns
+        {
+            get
+            {
+                int output = (int)Math.Ceiling(Math.Sqrt(ImagePreviews.Count));
+                return output;
+            }
+        }
+
+        public int ImageRows
+        {
+            get
+            {
+                int output = (int)Math.Ceiling(((double)ImagePreviews.Count) / ((double)ImageColumns));
+                return output;
+            }
+        }
+
+        public TagPicker(String[] secondaryTags, List<System.IO.FileInfo> previewImages)
+        {
+            InitializeComponent();
+
+            this.secondayTagListBox.ItemsSource = secondaryTags;
+            if (previewImages != null)
+            {
+                if (previewImages.Count > 16)
+                {
+                    previewImages.RemoveRange(16, previewImages.Count - 16);
+                }
+                ImagePreviews = new List<PreviewImage>();
+                foreach (FileInfo previewImage in previewImages)
+                {
+                    previewImage.Refresh();
+                    if (previewImage.Exists)
+                    {
+                        BitmapImage image = new BitmapImage();
+                        image.BeginInit();
+                        image.UriSource = new Uri(previewImage.FullName);
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.EndInit();
+                        image.Freeze();
+
+                        if(image.Width>2000)
+                        {
+                            image = CreateThumbnail(image);
+                        }
+
+                        PreviewImage img = new PreviewImage();
+                        img.Image = image;
+                        img.Name = previewImage.Name;
+                        this.ImagePreviews.Add(img);
+                    }
+                }
+            }
+
+            previewImageItems.DataContext = this;
+        }
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        private BitmapImage CreateThumbnail(BitmapImage image)
+        {
+                int width = 2000;
+
+                double old_height = image.Height;
+                double old_width = image.Width;
+                double ratio = width / old_width;
+                int new_height = (int)Math.Round(ratio * image.Height);
+
+                using (System.Drawing.Image thumbNail =
+                    new Bitmap(width, new_height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb))
+                {
+                    using (Graphics g = Graphics.FromImage(thumbNail))
+                    {
+                        g.CompositingQuality = CompositingQuality.HighQuality;
+                        g.SmoothingMode = SmoothingMode.HighQuality;
+                        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, width, new_height);
+                        g.DrawImage(BitmapImage2Bitmap(image), rect);
+                    }
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        thumbNail.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+
+                        ms.Seek(0, SeekOrigin.Begin);
+                        BitmapImage bi = new BitmapImage();
+                        bi.BeginInit();
+                        bi.CacheOption = BitmapCacheOption.OnLoad;
+                        bi.StreamSource = ms;
+                        bi.EndInit();
+                        bi.Freeze();
+                        return bi;
+                    }
+                }
+
+        }
+
+
+        public String SelectedTag
+        {
+            get
+            {
+                if (listRadio.IsChecked.GetValueOrDefault(false))
+                {
+                    if (this.secondayTagListBox.SelectedItem == null)
+                        return String.Empty;
+                    return this.secondayTagListBox.SelectedItem.ToString();
+                }else if (customRadio.IsChecked.GetValueOrDefault(false))
+                {
+                    if (string.IsNullOrWhiteSpace(customText.Text))
+                        return String.Empty;
+                    return customText.Text.Trim();
+                }
+                throw new Exception("Nor adio box selected");
+            }
+        }
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void secondayTagListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listRadio.IsChecked = true;
+        }
+
+        private void customText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(customText.Text))
+            customRadio.IsChecked = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.previewImageItems.DataContext = null;
+            this.ImagePreviews.Clear();
+        }
+
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Controls.Image imageControl = (System.Windows.Controls.Image)sender;
+            fullImage.Source = imageControl.Source;
+            fullImage.ToolTip = imageControl.ToolTip;
+            previewImageItems.Visibility = Visibility.Hidden;
+            fullImage.Visibility = Visibility.Visible;
+        }
+
+        private void fullImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            previewImageItems.Visibility = Visibility.Visible;
+            fullImage.Visibility = Visibility.Hidden;
+
+        }
+    }
+}
